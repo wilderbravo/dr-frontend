534aa665b016c8617180e19a3317c6eaec744dfd
Wilder Bravo <wilderbravoc@gmail.com>
2023-08-02 20:02:16 -0500

    Agregar vue-router y pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
+import { createPinia } from 'pinia'
 
-const app = createApp(App)
+const app = createApp(App).use(createPinia())
 installElementPlus(app)
 app.mount('#app')
