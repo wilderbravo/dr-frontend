1e24b964fb820c784186b80731546dc9058fcbc2
Wilder Bravo <wilderbravoc@gmail.com>
2023-08-02 19:53:21 -0500

    Agregar funcionalidad para presentar los resultados de los tickets aereos

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">  
     <FlightSearch @search="showFlightResults" />
+    <FlightResults :flights="searchResults" v-if="showResults" />
   </div>
 </template>
 
 <script>
 import FlightSearch from './components/FlightSearch.vue';
+import FlightResults from './components/FlightResults.vue';
 
 export default {
   name: 'App',
   components: {
-    FlightSearch
-  }
+    FlightSearch,
+    FlightResults
+  },
+   data() {
+    return {
+      searchResults: [],
+      showResults: false,
+    };
+  },
+  methods: {
+     showFlightResults() {
+      // Utilizamos los valores de los campos de búsqueda para simular los resultados de búsqueda
+      this.searchResults = [
+        {
+          id: 1,
+          price: "$250",
+          airline: "Aerolínea 1",
+          stops: 1,
+          duration: "3h 30m",
+        },
+        {
+          id: 2,
+          price: "$350",
+          airline: "Aerolínea 2",
+          stops: 0,
+          duration: "2h 15m",
+        },
+        {
+          id: 3,
+          price: "$236",
+          airline: "Aerolínea 3",
+          stops: 1,
+          duration: "3h 10m",
+        },
+        // Agrega más resultados aquí...
+      ];
+      this.showResults = true;
+    },
+  },
 }
 </script>
 
---
+++ src/components/FlightResults.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Resultados de búsqueda</h2>
+    <div v-if="flights.length > 0">
+      <div v-for="flight in flights" :key="flight.id" class="flight-card">
+        <div>Precio: {{ flight.price }}</div>
+        <div>Aerolínea: {{ flight.airline }}</div>
+        <div>Número de escalas: {{ flight.stops }}</div>
+        <div>Duración del vuelo: {{ flight.duration }}</div>
+      </div>
+    </div>
+    <div v-else>
+      <p>No se encontraron resultados</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    flights: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.flight-card {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
